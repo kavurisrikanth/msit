1309e9b4189fe0200462d8c19d8bc2f4d0fc0eb9
Srikanth Kavuri <ksrs9312@gmail.com>
2017-08-31 11:11:24 +0530

    Update typecast.c
    Added comments and cleaned the code up.

--- typecast.c
+++ typecast.c
@@ -1,12 +1,19 @@
+/*
+Is it possible to typecast a struct into a union (or vice versa)?
+Kavuri Srikanth
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
+// Created a struct.
 struct test_struct {
     int a;
     float b;
     // float c;
 };
 
+// Created a union.
 union test_union {
     int a;
     float b;
@@ -14,32 +21,28 @@ union test_union {
 };
 
 int main(void) {
+    
+    /* Using pointers.
+        This seems to be OK. */
     struct test_struct *s = calloc(1, sizeof(struct test_struct));
     s->b = 12.5;
     s->a = 10;
     printf("%d %.2f\n", s->a, s->b);
     
+    union test_union *u = (union test_union *)s;
+    printf("%d %f\n", u->a, u->b);
+    free(u);
+    
+    /* Using non-pointer variables.
+       This is throwing a compilation error, but it isn't explicitly telling me that the cast is not allowed. */
     // struct test_struct s;
     // s.a = 10;
     // s.b = 12.5;
     // printf("%d %.2f\n", s.a, s.b);
-    
 
-    union test_union u2;
-    u2.a = 20;
-    struct test_struct s2 = (struct test_struct)u2;
-    printf("%d\n", s2.a);
-
-    union test_union u = (union test_union)s;
-    printf("%d\n", u.a);
+    //union test_union u = (union test_union)s;
+    //printf("%d\n", u.a);
     // printf("%d %f\n", u.a, u.b);
-    
-    // union test_union *u = (union test_union *)s;
-    // printf("%d %f\n", u->a, u->b);
-    
-    // free(u);
-    
-    // printf("%d\n", u.a);
     
     return 0;
 }
